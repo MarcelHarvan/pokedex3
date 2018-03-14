e053cf0bf6192e7a02461d898f103bd256258898
Marcel Harvan <marcel.harvan@gmail.com>
2018-03-14 14:53:52 -0400

    Audio + view

---
+++ Pokedex3/Model/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  Pokedex3
+//
+//  Created by Marcel Harvan on 2018-03-12.
+//  Copyright © 2018 The Marcel's fake Company. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
---
+++ Pokedex3/View/PokeCell.swift
@@ -0,0 +1,27 @@
+//
+//  PokeCell.swift
+//  Pokedex3
+//
+//  Created by Marcel Harvan on 2018-03-13.
+//  Copyright © 2018 The Marcel's fake Company. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
