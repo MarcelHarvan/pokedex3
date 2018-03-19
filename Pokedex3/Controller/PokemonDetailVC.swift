125bee7055eb9e66485b824f3543e148b8cb8904
Marcel Harvan <marcel.harvan@gmail.com>
2018-03-19 15:19:22 -0400

    DetailVC connection to its class

--- Pokedex3/Controller/PokemonDetailVC.swift
+++ Pokedex3/Controller/PokemonDetailVC.swift
@@ -12,6 +12,21 @@ class PokemonDetailVC: UIViewController {
     
     var pokemon: Pokemon!
     @IBOutlet weak var nameLab: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var hightLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var weightLBL: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var defenceLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
